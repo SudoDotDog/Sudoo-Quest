203755773e10d4dd6ec45ad142b7ddced1644a5f
WMXPY <wm@mengw.io>
2021-06-26 11:52:17 -0500

    improve quest deistributer

--- src/distributer.ts
+++ src/distributer.ts
@@ -4,6 +4,32 @@
  * @description Distributer
  */
 
-export class QuestDistributer {
+import { QuestRequirementFunction } from "./declare";
+import { QuestRequirement } from "./requirement";
 
+export class QuestDistributer<Args extends any[] = []> {
+
+    public static create<Args extends any[] = []>(): QuestDistributer<Args> {
+
+        return new QuestDistributer<Args>();
+    }
+
+    private readonly _requirements: Array<QuestRequirement<Args>>;
+
+    private constructor() {
+
+        this._requirements = [];
+    }
+
+    public requires(description: string, requirement: QuestRequirementFunction<Args>): this {
+
+        const requirementInstance: QuestRequirement<Args> = QuestRequirement.create(description, requirement);
+        return this.addRequirement(requirementInstance);
+    }
+
+    public addRequirement(requirement: QuestRequirement<Args>): this {
+
+        this._requirements.push(requirement);
+        return this;
+    }
 }
